f5fa69fc06cc228fd571ece033f6db28fb3d48e6
Thinker Lee <thinker@codemud.net>
2020-09-11 08:49:14 -0700

    Send notifications only for chagnes.

--- targetsports.sh
+++ targetsports.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 PY=$(dirname $0)/targetsports.py
+OLD_CONTENT=
 while true; do
     TM=$(date '+%H:%M.%S')
     echo "$TM Check ammo"
     AVAIL=$(python3 $PY)
     if [ -n "$AVAIL" ]; then
         echo "$AVAIL"
-        notify-send "Ammo Available ($TM)" "$AVAIL"
+        if [ x"$OLD_CONTENT" != x"$AVAIL" ]; then
+            notify-send "Ammo Available ($TM)" "$AVAIL"
+        fi
     fi
+    OLD_CONTENT="$AVAIL"
     sleep 307
 done
